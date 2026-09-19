1ae366458dcbce240ae7a547642e8f8ce0f49230
Hex27 <leothepro555@gmail.com>
2026-09-19 16:08:52 +0800

    Initial

---
+++ extension.js
@@ -0,0 +1,21 @@
+/**
+ * called when extension is loaded
+ */
+function init(extensionMeta) {
+  //extensionMeta holds your metadata.json info
+
+}
+
+/**
+ * called when extension is loaded
+ */
+function enable() {
+
+}
+
+/**
+ * called when extension gets disabled
+ */
+function disable() {
+
+}
